25e667b7504735445e8d06a338a4c12900356afc
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-05-06 14:58:51 +0300

    Перенёс секретные настройки в .env

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,24 @@
 import os
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': 'checkpoint.devman.org',
         'PORT': '5434',
         'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
